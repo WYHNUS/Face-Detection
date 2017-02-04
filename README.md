16d9668d4f6210c63b599c08e923ad721e155e2a
WYHNUS <wangyanhao1993@163.com>
2017-02-04 21:33:57 +0800

    make sure demo works :)

---
+++ README.md
@@ -0,0 +1,19 @@
+# Face Detection
+
+Face detection using OpenCV, Node.js and Express.js
+
+## Dependency
+
+- [Node.js](https://nodejs.org/en/)
+- [OpenCV 2.4](http://opencv.org/)
+
+## Setup
+
+To test the feature on local computer:
+
+```
+npm install
+npm start
+```
+
+Open your browser and access the site via `localhost:8088`
--- app.js
+++ app.js
@@ -6,12 +6,12 @@ var multer = require('multer');
 var cv = require('opencv');
 
 var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'public/tmp/uploads/')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now())
-  }
+	destination: function (req, file, cb) {
+		cb(null, 'public/tmp/uploads/')
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.fieldname + '-' + Date.now())
+	}
 });
 var upload = multer(
 	{ storage: storage }
@@ -19,11 +19,6 @@ var upload = multer(
 
 var port = 8088;
 var app = express();
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -33,36 +28,49 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.post('/processImage', upload.single('camera-input'), function(req, res) {
-	console.log(req.file);
-	cv.readImage(req.file.path, function(err, img) {
-		if (err) {
-			console.log(err);
-		} else {
-			const width = img.width();
-			const height = img.height();
-			if (width < 1 || height < 1) {
-				console.log('Image has no size');
-			} else {
-				img.detectObject(cv.FACE_CASCADE, {}, function(err, faces) {
-					if (err) {
-						console.log(err);
-					} else {
-						console.log(faces);
-					}
-				});
-			}
-		}
-	});
-});
-
-app.use('/', function(req, res) {
+// entry page
+app.get('/', function(req, res) {
 	res.render('home', {
 		scripts: ['javascripts/displayImage.js']
 	});
 });
 
+// display processed image
+app.post('/', upload.single('camera-input'), function(req, res) {
+	cv.readImage(req.file.path, function(err, img) {
+		if (err) {
+			resError(res, err);
+		} else if (img.width() < 1 || img.height() < 1) {
+			resError(res, new Error('Image has no size'));
+		} else {
+			img.detectObject(cv.FACE_CASCADE, {}, function(err, faces) {
+				if (err) {
+					resError(res, err);
+				} else {
+					for (var i in faces) {
+						var face = faces[i];
+  						img.ellipse(face.x + face.width/2, face.y + face.height/2, 
+  									face.width/2, face.height/2);
+					}
+					var imgUrl = '/tmp/uploads/' + req.file.filename.replace('input', 'output') + '.jpg';
+					img.save('public' + imgUrl);
+					res.render('result', {
+						detectedFaces: faces.length,
+						imageUrl: imgUrl
+					});
+				}
+			});
+		}
+	});
+});
+
+function resError(res, err) {
+    return res.status(500).json({
+        message: err.message
+    });
+}
+
 http.createServer(app)
-  .listen(port, function(server) {
-    console.log('Listening on port %d', port);
-  });
+	.listen(port, function(server) {
+		console.log('Listening on port %d', port);
+	});
